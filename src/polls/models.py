75f616325c2287296257a5713cad35616ae31c3e
Evgeny Ivanov <Saboteuur@yandex.ru>
2017-02-27 23:55:54 +0300

    part 6 passed

--- src/polls/models.py
+++ src/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -9,6 +11,10 @@ class Question(models.Model):
 
     def my_custom_function(self):
         return self.question_text + " custom"
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
